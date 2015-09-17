524d468f20c6d53b18e1b161a9aa0efaf7d2edd4
Vincent Wochnik <v.wochnik@gmail.com>
2015-09-17 21:46:47 +0200

    site outline

--- js/site.js
+++ js/site.js
@@ -1,27 +1,8 @@
 (function($, wnd) {
   $(function() {
-    $('.navbar-toggle').click(function() {
-      $('.navbar').toggleClass('toggled');
-    });
-
     /* adjust height */
-    $('.viewport-adjust-height').viewportAdjust(function(elem, height) {
+    $('.viewport-adjust').viewportAdjust(function(elem, height) {
       return $(elem).height(height).height();
-    });
-    $('.viewport-adjust-height-half').viewportAdjust(function(elem, height) {
-      return $(elem).height(height / 2.0).height() * 2.0;
-    });
-
-    /* adjust logo clip */
-    $('.logo-clip-inside.viewport-adjust-clip-height').viewportAdjust(function(elem, height) {
-      var rect = '(0 '+$(wnd).width()+'px '+height+'px 0)';
-      $(elem).css({'clip': 'rect'+rect, 'clip-path': 'inset'+rect});
-      return height;
-    });
-    $('.logo-clip-inside.viewport-adjust-clip-height-half').viewportAdjust(function(elem, height) {
-      var rect = '(0 '+$(wnd).width()+'px '+(height / 2.0)+'px 0)';
-      $(elem).css({'clip': 'rect'+rect, 'clip-path': 'inset'+rect});
-      return height;
     });
   });
 })(jQuery, window);
