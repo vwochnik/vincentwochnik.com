3d2407f7144943240ce0e0e871dc6f310d5bc43b
Vincent Wochnik <v.wochnik@gmail.com>
2015-09-18 17:57:18 +0200

    add features

--- js/site.js
+++ js/site.js
@@ -1,8 +1,21 @@
 (function($, wnd) {
   $(function() {
-    /* adjust height */
+    /* adjust cover height */
     $('.viewport-adjust').viewportAdjust(function(elem, height) {
       return $(elem).height(height).height();
+    });
+
+    /* arrow down scroll */
+    $('a.animated-scroll').each(function() {
+      var $this = $(this)
+          $root = $('html, body'),
+          href = $this.attr('href');
+
+        $this.click(function() {
+          $root.animate({ scrollTop: $(href).offset().top }, 500,
+            function() { wnd.location.hash = href; });
+          return false;
+        });
     });
   });
 })(jQuery, window);
