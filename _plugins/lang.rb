bfd67084a2d250566846fe19669896909bfc56da
Vincent Wochnik <v.wochnik@gmail.com>
2015-11-11 23:42:40 +0100

    language config

--- _plugins/lang.rb
+++ _plugins/lang.rb
@@ -6,10 +6,18 @@ module Jekyll
       @lkey = @params.shift
     end
 
-    def render(context)
+    def get_language_data(context)
       lang = context['page']['lang']
-      data = context['site']['data']['lang'][lang]
 
+      language_data = context['site']['language_data'].strip
+      traverse = language_data.gsub("%%", lang).split('.')
+      data = context['site']
+      traverse.each { |t| data = data[t] }
+      data
+    end
+
+    def render(context)
+      data = get_language_data(context)
       a = context['page']['alias']
       if a and data.include?(a) and data[a].include?(@lkey)
         str = "#{data[a][@lkey]}"
