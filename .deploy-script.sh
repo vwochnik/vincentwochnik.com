1b28d1fb9e5baba2ed33d3d16ee5d4b4f9ac32f3
Vincent Wochnik <v.wochnik@gmail.com>
2016-02-28 00:22:29 +0100

    update deploy script to deploy to gh-pages

--- .deploy-script.sh
+++ .deploy-script.sh
@@ -11,8 +11,8 @@ pushd $TMP_DIR
 git init
 git add .
 git commit -m "Site built at $DATE"
-git remote add origin git@github.com:vwochnik/vwochnik.github.io.git
-git push -fu origin master
+git remote add origin git@github.com:vwochnik/vincentwochnik.com.git
+git push -fu origin gh-pages
 
 popd
 
