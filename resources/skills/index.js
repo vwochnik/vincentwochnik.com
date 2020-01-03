11fc20a480bf58862a15bfc69863f3f827273a10
Vincent Wochnik <v.wochnik@gmail.com>
2020-01-03 13:29:55 +0100

    add skills list

--- resources/skills/index.js
+++ resources/skills/index.js
@@ -25,12 +25,12 @@ function generateSkill(label, rating, fontSize, outputFile) {
 		.attr("xmlns", "http://www.w3.org/2000/svg");
 
 	const background = svg.append('path')
-	  .attr('d', d3.arc().innerRadius(radius*0.9).outerRadius(radius).startAngle(0).endAngle(2*Math.PI))
+	  .attr('d', d3.arc().innerRadius(radius*0.85).outerRadius(radius).startAngle(0).endAngle(2*Math.PI))
 	  .attr("transform", `translate(${width/2},${height/2})`)
 	  .attr("fill", "rgba(121,168,169,0.2)");
 
 	const skill = svg.append('path')
-	  .attr('d', d3.arc().innerRadius(radius*0.9).outerRadius(radius).startAngle(0).endAngle(rating*2*Math.PI))
+	  .attr('d', d3.arc().innerRadius(radius*0.85).outerRadius(radius).startAngle(0).endAngle(rating*2*Math.PI))
 	  .attr("transform", `translate(${width/2},${height/2})`)
 	  .attr("fill", "#79a8a9");
 
@@ -39,12 +39,21 @@ function generateSkill(label, rating, fontSize, outputFile) {
 		.text(label)
 		.attr("font-family", "sans-serif")
 		.attr("font-size", fontSize)
+		.attr("font-weight", "bold")
 		.attr("text-anchor", "middle")
 		.attr("dy", ".35em")
 		.attr("fill", "#79a8a9");
 
 	// Output the result to file
 	fs.writeFileSync(outputFile, container.html());
+
+	console.info(`<li><img src="${outputFile}" alt="${label}"></li>`)
 }
 
-generateSkill("HTML/CSS", 1.0, 40, "skill-html.svg");
+generateSkill("HTML5", 1.0, 45, "skill-html.svg");
+generateSkill("CSS3", 0.8, 41, "skill-css.svg");
+generateSkill("JavaScript/ES6", 0.9, 28, "skill-js.svg");
+generateSkill("Node.js", 0.9, 40, "skill-node.svg");
+generateSkill("React.js", 0.8, 40, "skill-react.svg");
+generateSkill("D3.js", 0.8, 40, "skill-d3.svg");
+generateSkill("Go", 0.7, 40, "skill-go.svg");
